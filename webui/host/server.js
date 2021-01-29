66fa07dda75271e5bc2e400d2fc2aa3ce21664b1
prateekcool007 <prateek.shrivastava5@gmail.com>
2021-01-29 15:19:17 +1100

    Fixed path dependency

--- webui/host/server.js
+++ webui/host/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const expressApp = express();
+const path = require('path');
+
 const port = 8989;
 
 expressApp.use(express.json());
@@ -7,5 +9,5 @@ expressApp.use('/pub', express.static(path.join(__dirname, '..\public')));
 
 
 expressApp.listen(port, () => {
-console.log('WebHost running at http://localhost:${port}');
+console.log(`WebHost running at http://localhost:${port}`);
 });
