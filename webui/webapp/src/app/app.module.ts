b898ca0046df41f7d09f568f45f8b8349e48ede6
prateekcool007 <prateek.shrivastava5@gmail.com>
2021-02-15 22:15:14 +1100

    Sample template for home page

--- webui/webapp/src/app/app.module.ts
+++ webui/webapp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CategoryCardsComponent } from './category-cards/category-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    CategoryCardsComponent
   ],
   imports: [
     BrowserModule,
